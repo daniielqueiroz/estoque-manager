b3d64a733b029086883c323922170b91e1f99581
João Daniel <56694251+daniielqueiroz@users.noreply.github.com>
2026-03-09 19:03:37 -0300

    feat: configurando cors na aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import { router } from "./routers";
 import { errorHandler } from "./shared/middlewares/errorHandler";
@@ -7,6 +8,7 @@ function createApp() {
   const app = express();
 
   app.use(express.json());
+  app.use(cors());
   app.use("/api", router);
   app.use(errorHandler);
 
